a53eecc0db7915ea58b75ed51814deb2e8e66e88
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-03-26 02:22:24 +0000

    Update script to calculate and display CPU usage.

--- server-stats.sh
+++ server-stats.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# script to analyze basic server performance stats on Ubuntu systems
+#
+# Display total CPU usage, total memory usage, total disk usage,
+# top 5 processes by CPU usage, top 5 processes by memory usage.
 
-while true
-do
-    clear
-    echo "This is a test message"
-    sleep 1
+while true; do
+  clear
+  # The awk tool fetches the 8th field from the line i.e. idle percentage.
+  idle=$(top -b -n 1 | grep -- "%Cpu" | awk '{print $8}')
+  usage=$(echo "100 - $idle" | bc)
+  echo "CPU Usage: $usage%"
+  sleep 1
 done
